90e6fa89b8c13029e72d99ace91e15c0011223da
kawayama <kawa130307@gmail.com>
2025-12-24 02:38:54 +0900

    chore: update pubspec.yaml version to 0.1.0+12, modify Google Maps API key reference, and add location usage descriptions in Info.plist

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    guard let googleMapApiKey = Bundle.main.infoDictionary?["GOOGLE_MAP_API_KEY"] as? String else {
+    guard let googleMapApiKey = Bundle.main.infoDictionary?["GoogleMapsApiKey"] as? String else {
       fatalError("GoogleMapsApiKey is not set in Info.plist")
     }
     GMSServices.provideAPIKey(googleMapApiKey)
