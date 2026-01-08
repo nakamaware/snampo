94e5fec58b8ab913e81b862c60b73a35277f9a6a
sekkoh <72484680+sekkoh@users.noreply.github.com>
2026-01-08 21:40:35 +0900

    chore: 依存関係を更新

--- frontend/lib/core/network/dio_provider.g.dart
+++ frontend/lib/core/network/dio_provider.g.dart
@@ -6,23 +6,49 @@ part of 'dio_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$dioHash() => r'580e9ac0809f0f8c5d38ead2bad1d2f32d462321';
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+/// Dioインスタンスを提供するプロバイダー
+
+@ProviderFor(dio)
+final dioProvider = DioProvider._();
 
 /// Dioインスタンスを提供するプロバイダー
-///
-/// Copied from [dio].
-@ProviderFor(dio)
-final dioProvider = AutoDisposeProvider<Dio>.internal(
-  dio,
-  name: r'dioProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$dioHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef DioRef = AutoDisposeProviderRef<Dio>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+final class DioProvider extends $FunctionalProvider<Dio, Dio, Dio>
+    with $Provider<Dio> {
+  /// Dioインスタンスを提供するプロバイダー
+  DioProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'dioProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$dioHash();
+
+  @$internal
+  @override
+  $ProviderElement<Dio> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  Dio create(Ref ref) {
+    return dio(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(Dio value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<Dio>(value),
+    );
+  }
+}
+
+String _$dioHash() => r'580e9ac0809f0f8c5d38ead2bad1d2f32d462321';
--- frontend/lib/features/mission/presentation/store/mission_store.g.dart
+++ frontend/lib/features/mission/presentation/store/mission_store.g.dart
@@ -6,141 +6,334 @@ part of 'mission_store.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$missionRepositoryHash() => r'2392a60bfd7b3e1c8bf62814038ee5f2df98a180';
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+/// ミッションリポジトリのプロバイダー
+
+@ProviderFor(missionRepository)
+final missionRepositoryProvider = MissionRepositoryProvider._();
 
 /// ミッションリポジトリのプロバイダー
-///
-/// Copied from [missionRepository].
-@ProviderFor(missionRepository)
-final missionRepositoryProvider =
-    AutoDisposeProvider<MissionRepository>.internal(
-  missionRepository,
-  name: r'missionRepositoryProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$missionRepositoryHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef MissionRepositoryRef = AutoDisposeProviderRef<MissionRepository>;
+final class MissionRepositoryProvider extends $FunctionalProvider<
+    MissionRepository,
+    MissionRepository,
+    MissionRepository> with $Provider<MissionRepository> {
+  /// ミッションリポジトリのプロバイダー
+  MissionRepositoryProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'missionRepositoryProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$missionRepositoryHash();
+
+  @$internal
+  @override
+  $ProviderElement<MissionRepository> $createElement(
+          $ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  MissionRepository create(Ref ref) {
+    return missionRepository(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(MissionRepository value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<MissionRepository>(value),
+    );
+  }
+}
+
+String _$missionRepositoryHash() => r'2392a60bfd7b3e1c8bf62814038ee5f2df98a180';
+
+/// 現在地を取得するユースケースのプロバイダー
+
+@ProviderFor(getCurrentLocationUseCase)
+final getCurrentLocationUseCaseProvider = GetCurrentLocationUseCaseProvider._();
+
+/// 現在地を取得するユースケースのプロバイダー
+
+final class GetCurrentLocationUseCaseProvider extends $FunctionalProvider<
+    GetCurrentLocationUseCase,
+    GetCurrentLocationUseCase,
+    GetCurrentLocationUseCase> with $Provider<GetCurrentLocationUseCase> {
+  /// 現在地を取得するユースケースのプロバイダー
+  GetCurrentLocationUseCaseProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'getCurrentLocationUseCaseProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$getCurrentLocationUseCaseHash();
+
+  @$internal
+  @override
+  $ProviderElement<GetCurrentLocationUseCase> $createElement(
+          $ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  GetCurrentLocationUseCase create(Ref ref) {
+    return getCurrentLocationUseCase(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(GetCurrentLocationUseCase value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<GetCurrentLocationUseCase>(value),
+    );
+  }
+}
+
 String _$getCurrentLocationUseCaseHash() =>
     r'1b46d72dd802296b8ce7b3f16ac4daac57905cc1';
 
-/// 現在地を取得するユースケースのプロバイダー
-///
-/// Copied from [getCurrentLocationUseCase].
-@ProviderFor(getCurrentLocationUseCase)
-final getCurrentLocationUseCaseProvider =
-    AutoDisposeProvider<GetCurrentLocationUseCase>.internal(
-  getCurrentLocationUseCase,
-  name: r'getCurrentLocationUseCaseProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$getCurrentLocationUseCaseHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+/// ミッション情報を取得するユースケースのプロバイダー
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef GetCurrentLocationUseCaseRef
-    = AutoDisposeProviderRef<GetCurrentLocationUseCase>;
-String _$getMissionUseCaseHash() => r'7b18f274819f9f458415aa5c5156604e70ec641b';
+@ProviderFor(getMissionUseCase)
+final getMissionUseCaseProvider = GetMissionUseCaseProvider._();
 
 /// ミッション情報を取得するユースケースのプロバイダー
-///
-/// Copied from [getMissionUseCase].
-@ProviderFor(getMissionUseCase)
-final getMissionUseCaseProvider =
-    AutoDisposeProvider<GetMissionUseCase>.internal(
-  getMissionUseCase,
-  name: r'getMissionUseCaseProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$getMissionUseCaseHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef GetMissionUseCaseRef = AutoDisposeProviderRef<GetMissionUseCase>;
-String _$targetHash() => r'3846633866fe443b5960ebafaa7e4e5a1f74d2c2';
+final class GetMissionUseCaseProvider extends $FunctionalProvider<
+    GetMissionUseCase,
+    GetMissionUseCase,
+    GetMissionUseCase> with $Provider<GetMissionUseCase> {
+  /// ミッション情報を取得するユースケースのプロバイダー
+  GetMissionUseCaseProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'getMissionUseCaseProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
 
-/// 目的地を取得するプロバイダー
-///
-/// Copied from [target].
-@ProviderFor(target)
-final targetProvider = AutoDisposeProvider<MidPointEntity?>.internal(
-  target,
-  name: r'targetProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$targetHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+  @override
+  String debugGetCreateSourceHash() => _$getMissionUseCaseHash();
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef TargetRef = AutoDisposeProviderRef<MidPointEntity?>;
-String _$routeHash() => r'166b7fe15f3010294955f9fa3143ea182b613da6';
+  @$internal
+  @override
+  $ProviderElement<GetMissionUseCase> $createElement(
+          $ProviderPointer pointer) =>
+      $ProviderElement(pointer);
 
-/// ルートのポリライン文字列を取得するプロバイダー
-///
-/// Copied from [route].
-@ProviderFor(route)
-final routeProvider = AutoDisposeProvider<String?>.internal(
-  route,
-  name: r'routeProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$routeHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+  @override
+  GetMissionUseCase create(Ref ref) {
+    return getMissionUseCase(ref);
+  }
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef RouteRef = AutoDisposeProviderRef<String?>;
-String _$midpointInfoListHash() => r'bd03c204e42e81369e0ba38cf6f12a0239877555';
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(GetMissionUseCase value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<GetMissionUseCase>(value),
+    );
+  }
+}
 
-/// 中間地点のリストを取得するプロバイダー
-///
-/// Copied from [midpointInfoList].
-@ProviderFor(midpointInfoList)
-final midpointInfoListProvider =
-    AutoDisposeProvider<List<MidPointEntity>?>.internal(
-  midpointInfoList,
-  name: r'midpointInfoListProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$midpointInfoListHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+String _$getMissionUseCaseHash() => r'7b18f274819f9f458415aa5c5156604e70ec641b';
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef MidpointInfoListRef = AutoDisposeProviderRef<List<MidPointEntity>?>;
+/// ミッション情報を管理するストア
+
+@ProviderFor(MissionNotifier)
+final missionProvider = MissionNotifierProvider._();
+
+/// ミッション情報を管理するストア
+final class MissionNotifierProvider
+    extends $AsyncNotifierProvider<MissionNotifier, LocationEntity> {
+  /// ミッション情報を管理するストア
+  MissionNotifierProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'missionProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$missionNotifierHash();
+
+  @$internal
+  @override
+  MissionNotifier create() => MissionNotifier();
+}
+
 String _$missionNotifierHash() => r'1610d90005d51228d033c67e08315606d4acc54b';
 
 /// ミッション情報を管理するストア
-///
-/// Copied from [MissionNotifier].
-@ProviderFor(MissionNotifier)
-final missionNotifierProvider =
-    AutoDisposeAsyncNotifierProvider<MissionNotifier, LocationEntity>.internal(
-  MissionNotifier.new,
-  name: r'missionNotifierProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$missionNotifierHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
 
-typedef _$MissionNotifier = AutoDisposeAsyncNotifier<LocationEntity>;
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
+abstract class _$MissionNotifier extends $AsyncNotifier<LocationEntity> {
+  FutureOr<LocationEntity> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final ref = this.ref as $Ref<AsyncValue<LocationEntity>, LocationEntity>;
+    final element = ref.element as $ClassProviderElement<
+        AnyNotifier<AsyncValue<LocationEntity>, LocationEntity>,
+        AsyncValue<LocationEntity>,
+        Object?,
+        Object?>;
+    element.handleCreate(ref, build);
+  }
+}
+
+/// 目的地を取得するプロバイダー
+
+@ProviderFor(target)
+final targetProvider = TargetProvider._();
+
+/// 目的地を取得するプロバイダー
+
+final class TargetProvider extends $FunctionalProvider<MidPointEntity?,
+    MidPointEntity?, MidPointEntity?> with $Provider<MidPointEntity?> {
+  /// 目的地を取得するプロバイダー
+  TargetProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'targetProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$targetHash();
+
+  @$internal
+  @override
+  $ProviderElement<MidPointEntity?> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  MidPointEntity? create(Ref ref) {
+    return target(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(MidPointEntity? value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<MidPointEntity?>(value),
+    );
+  }
+}
+
+String _$targetHash() => r'3846633866fe443b5960ebafaa7e4e5a1f74d2c2';
+
+/// ルートのポリライン文字列を取得するプロバイダー
+
+@ProviderFor(route)
+final routeProvider = RouteProvider._();
+
+/// ルートのポリライン文字列を取得するプロバイダー
+
+final class RouteProvider extends $FunctionalProvider<String?, String?, String?>
+    with $Provider<String?> {
+  /// ルートのポリライン文字列を取得するプロバイダー
+  RouteProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'routeProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$routeHash();
+
+  @$internal
+  @override
+  $ProviderElement<String?> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  String? create(Ref ref) {
+    return route(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(String? value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<String?>(value),
+    );
+  }
+}
+
+String _$routeHash() => r'166b7fe15f3010294955f9fa3143ea182b613da6';
+
+/// 中間地点のリストを取得するプロバイダー
+
+@ProviderFor(midpointInfoList)
+final midpointInfoListProvider = MidpointInfoListProvider._();
+
+/// 中間地点のリストを取得するプロバイダー
+
+final class MidpointInfoListProvider extends $FunctionalProvider<
+    List<MidPointEntity>?,
+    List<MidPointEntity>?,
+    List<MidPointEntity>?> with $Provider<List<MidPointEntity>?> {
+  /// 中間地点のリストを取得するプロバイダー
+  MidpointInfoListProvider._()
+      : super(
+          from: null,
+          argument: null,
+          retry: null,
+          name: r'midpointInfoListProvider',
+          isAutoDispose: true,
+          dependencies: null,
+          $allTransitiveDependencies: null,
+        );
+
+  @override
+  String debugGetCreateSourceHash() => _$midpointInfoListHash();
+
+  @$internal
+  @override
+  $ProviderElement<List<MidPointEntity>?> $createElement(
+          $ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  List<MidPointEntity>? create(Ref ref) {
+    return midpointInfoList(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(List<MidPointEntity>? value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<List<MidPointEntity>?>(value),
+    );
+  }
+}
+
+String _$midpointInfoListHash() => r'bd03c204e42e81369e0ba38cf6f12a0239877555';
