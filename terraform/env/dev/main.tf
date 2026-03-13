b88f660f7df12400db9a3d6ed0824377a6cd237a
KineYue <kineyue@nakamaware.com>
2026-03-14 03:14:47 +0900

    feat: 1日あたりのRate Limitを追加
    以下の1日あたりのRate Limitを追加
    - Directions API
    - Places API (New)
    - Roads API
    - Street View Static API
    追加したRate Limitは現在アプリを使用した際に用いられるものだけである
    
    Ref: 152

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -45,3 +45,54 @@ module "snampo_dev" {
     },
   ]
 }
+
+resource "google_cloud_quotas_quota_preference" "overall_rate_limit_for_directions_api" {
+    service       = "directions-backend.googleapis.com"
+    quota_id      = "BillableDefaultPerDayPerProject"
+    parent        = "projects/snampo-480404"
+    name          = "Requests per day"
+    quota_config  {
+        preferred_value = 15000
+    }
+}
+
+resource "google_cloud_quotas_quota_preference" "overall_rate_limit_for_new_places_api" {
+    service       = "places.googleapis.com"
+    quota_id      = "SearchNearbyRequestPerDayPerProject"
+    parent        = "projects/snampo-480404"
+    name          = "SearchNearbyRequest per day"
+    quota_config  {
+        preferred_value = 15000
+    }
+}
+
+resource "google_cloud_quotas_quota_preference" "overall_rate_limit_for_roads_api" {
+    service       = "roads.googleapis.com"
+    quota_id      = "BillableDefaultPerDayPerProject"
+    parent        = "projects/snampo-480404"
+    name          = "Requests per day"
+    quota_config  {
+        preferred_value = 15000
+    }
+}
+# street-view-image-backend.googleapis.com (Street View Static API)
+# Directions APIのRate Limit
+resource "street-view-image-backend.googleapis.com" "overall_rate_limit_for_street_view_api_from_be" {
+    service       = "directions-backend.googleapis.com"
+    quota_id      = "StreetViewMetadataPerDayPerProject"
+    parent        = "projects/snampo-480404"
+    name          = "Metadata requests per day"
+    quota_config  {
+        preferred_value = 15000
+    }
+}
+
+resource "street-view-image-backend.googleapis.com" "overall_rate_limit_for_street_view_api_from_app" {
+    service       = "directions-backend.googleapis.com"
+    quota_id      = "BillableUnsignedbucketPerDayPerProject"
+    parent        = "projects/snampo-480404"
+    name          = "Unsigned requests (if URL signing secret is defined) per day"
+    quota_config  {
+        preferred_value = 15000
+    }
+}
