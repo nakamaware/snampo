c8fe54329035227c1c3187ae47ac85778f076ee2
HZW1713 <kou-tetsui-ry@ynu.jp>
2026-04-26 10:33:58 +0900

    fix: lint

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,10 +5,9 @@ import logging
 
 from fastapi import FastAPI
 
-from app.config import ENV
 from app.api.openapi import custom_openapi
 from app.api.routes import route
-
+from app.config import ENV
 
 LOG_LEVEL = logging.INFO if ENV == "prod" else logging.DEBUG
 logging.basicConfig(level=LOG_LEVEL)
